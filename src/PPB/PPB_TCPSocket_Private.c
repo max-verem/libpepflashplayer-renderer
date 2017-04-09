4722af341e16a169d2358789a93d8f2c05fc3c70
root <root@dev-5.internal.m1stereo.tv>
2017-04-09 07:54:52 -0400

    Minimal implementation of tcp socket

--- src/PPB/PPB_TCPSocket_Private.c
+++ src/PPB/PPB_TCPSocket_Private.c
@@ -5,19 +5,88 @@
 #include <ppapi/c/ppp.h>
 #include <ppapi/c/ppp_instance.h>
 #include <ppapi/c/pp_time.h>
+#include <ppapi/c/pp_errors.h>
 #include <ppapi/c/pp_completion_callback.h>
 
 #include <ppapi/c/private/ppb_tcp_socket_private.h>
 
+#include <pthread.h>
+#include <errno.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+
+#include "PPB_MessageLoop.h"
+
+#include "res.h"
 #include "log.h"
+
+static int32_t errno2pp(int e)
+{
+    int r = PP_ERROR_FAILED;
+
+    switch(e)
+    {
+        case EACCES:
+        case EPERM: return PP_ERROR_NOACCESS;
+        case EADDRINUSE: return PP_ERROR_ADDRESS_IN_USE;
+        case ECONNREFUSED: return PP_ERROR_CONNECTION_REFUSED;
+        case ENETUNREACH: return PP_ERROR_ADDRESS_UNREACHABLE;
+        case ETIMEDOUT: return PP_ERROR_CONNECTION_TIMEDOUT;
+        case ENOTCONN: return PP_ERROR_CONNECTION_CLOSED;
+        case ECONNRESET: return PP_ERROR_CONNECTION_RESET;
+        case EAGAIN:
+        case EBADF: return PP_ERROR_FAILED;
+        default: LOG_E("unhandled %d (%s)", e, strerror(e));
+    };
+
+    return r;
+};
+
+struct PPB_TCPSocket_Private_0_5 PPB_TCPSocket_Private_0_5_instance;
+
+typedef struct ppb_tcpsocket_private_desc
+{
+    uint64_t async, asyncs;
+    PP_Resource self;
+    PP_Instance instance_id;
+    pthread_mutex_t lock;
+    int s;
+} ppb_tcpsocket_private_t;
+
+static void ppb_tcpsocket_private_destructor(ppb_tcpsocket_private_t* ctx)
+{
+    LOG_D("{%d}", ctx->self);
+
+    /* destroy mutex */
+    pthread_mutex_destroy(&ctx->lock);
+};
+
 
 /**
  * Allocates a TCP socket resource.
  */
 static PP_Resource Create(PP_Instance instance)
 {
-    LOG_NP;
-    return 0;
+    ppb_tcpsocket_private_t* dst;
+    int res = res_create(sizeof(ppb_tcpsocket_private_t), &PPB_TCPSocket_Private_0_5_instance, (res_destructor_t)ppb_tcpsocket_private_destructor);
+
+    LOG_D("res=%d", res);
+
+    /* get private data */
+    dst = (ppb_tcpsocket_private_t*)res_private(res);
+
+    /* setup internals */
+    dst->instance_id = instance;
+    dst->self = res;
+
+    /* create mutex mutex */
+    pthread_mutex_init(&dst->lock, NULL);
+
+    return res;
 };
 
 /**
@@ -25,8 +94,7 @@ static PP_Resource Create(PP_Instance instance)
  */
 static PP_Bool IsTCPSocket(PP_Resource resource)
 {
-    LOG_NP;
-    return 0;
+    return (res_interface(resource) == &PPB_TCPSocket_Private_0_5_instance);
 };
 
 /**
@@ -34,12 +102,126 @@ static PP_Bool IsTCPSocket(PP_Resource resource)
  * When a proxy server is used, |host| and |port| refer to the proxy server
  * instead of the destination server.
  */
+typedef struct ConnectArgsDesc
+{
+    uint64_t async;
+    ppb_tcpsocket_private_t* ctx;
+    char* host;
+    uint16_t port;
+    struct PP_CompletionCallback callback;
+} ConnectArgs_t;
+
+static void* ConnectProcAsync(void* a)
+{
+    char tmp[8192];
+    int r, hp_errno;
+    struct hostent hostbuf, *host_ip;
+    ConnectArgs_t* args = (ConnectArgs_t*)a;
+
+    /* resolv hostname */
+    r = gethostbyname_r(args->host, &hostbuf, tmp, sizeof(tmp), &host_ip, &hp_errno);
+    if(!host_ip)
+    {
+        LOG_E("gethostbyname(%s) failed", args->host);
+
+        /* send callback */
+        pthread_mutex_lock(&args->ctx->lock);
+        args->ctx->asyncs--;
+        PPB_MessageLoop_push(0, args->callback, 0, PP_ERROR_NAME_NOT_RESOLVED);
+        pthread_mutex_unlock(&args->ctx->lock);
+    }
+    else
+    {
+        int s;
+
+        /* create communication socket */
+        s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if(s < 0)
+        {
+            s = errno;
+
+            LOG_E("socket() failed, errno=%d", s);
+
+            /* send callback */
+            pthread_mutex_lock(&args->ctx->lock);
+            args->ctx->asyncs--;
+            PPB_MessageLoop_push(0, args->callback, 0, errno2pp(s));
+            pthread_mutex_unlock(&args->ctx->lock);
+        }
+        else
+        {
+            struct sockaddr_in s_local;
+            struct sockaddr_in s_remote;
+
+            /* prepare address */
+            s_remote.sin_family = AF_INET;
+            s_remote.sin_addr.s_addr = inet_addr(inet_ntoa(*(struct in_addr*)(host_ip->h_addr_list[0])));
+            s_remote.sin_port = htons((unsigned short)args->port);
+
+            /* connect */
+            r = connect(s, (struct sockaddr*)&s_remote, sizeof(s_remote));
+            if(r < 0)
+            {
+                r = errno;
+
+                LOG_E("connect(%s:%d) failed, errno=%d (%s)", args->host, args->port, r, strerror(r));
+
+                /* send callback */
+                pthread_mutex_lock(&args->ctx->lock);
+                args->ctx->asyncs--;
+                PPB_MessageLoop_push(0, args->callback, 0, errno2pp(r));
+                pthread_mutex_unlock(&args->ctx->lock);
+
+                close(s);
+            }
+            else
+            {
+                int f;
+
+                /* set socket option */
+                f = 1; r = setsockopt(s, IPPROTO_TCP, TCP_NODELAY, (char*)&f, sizeof(f));
+
+                /* setup SO_KEEPALIVE for socket */
+                f = 1; r = setsockopt(s, SOL_SOCKET, SO_KEEPALIVE, (char*)&f, sizeof(f));
+
+                LOG_N("socket(%s:%d)=%d", args->host, args->port, s);
+
+                /* send callback */
+                pthread_mutex_lock(&args->ctx->lock);
+                args->ctx->s = s;
+                args->ctx->asyncs--;
+                PPB_MessageLoop_push(0, args->callback, 0, errno2pp(r));
+                pthread_mutex_unlock(&args->ctx->lock);
+            };
+        };
+    };
+
+    free(args->host);
+    free(args);
+
+    return NULL;
+};
+
 static int32_t Connect(PP_Resource tcp_socket,
     const char* host, uint16_t port,
     struct PP_CompletionCallback callback)
 {
-    LOG_NP;
-    return 0;
+    pthread_t th;
+    ConnectArgs_t* args;
+
+    args = (ConnectArgs_t*)calloc(1, sizeof(ConnectArgs_t));
+    args->host = strdup(host);
+    args->port = port;
+    args->callback = callback;
+    args->ctx = (ppb_tcpsocket_private_t*)res_private(tcp_socket);
+    pthread_mutex_lock(&args->ctx->lock);
+    args->async = ++args->ctx->async;
+    args->ctx->asyncs++;
+    pthread_create(&th, NULL, ConnectProcAsync, args);
+    pthread_detach(th);
+    pthread_mutex_unlock(&args->ctx->lock);
+
+    return PP_OK_COMPLETIONPENDING;
 };
 
 /**
